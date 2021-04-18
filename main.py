a93da63b4929473701ad816f8dd60a5b16f8dd8b
rajjaiswal07 <rajjaiswal07.rj@gmail.com>
2021-04-18 19:48:57 +0530

    error partially solved

--- main.py
+++ main.py
@@ -66,61 +66,71 @@ def prediction_result(top, aplcnt_name, cv_path, personality_values):
     personality = model.test(personality_values)
     print("\n############# Predicted Personality #############\n")
     print(personality)
-    data = ResumeParser(cv_path).get_extracted_data()
-    
-    try:
-        del data['name']
-        if len(data['mobile_number'])<10:
-            del data['mobile_number']
-    except:
-        pass
-    
-    print("\n############# Resume Parsed Data #############\n")
+    print(aplcnt_name.get())
+    print(age)
 
-    for key in data.keys():
-        if data[key] is not None:
-            print('{} : {}'.format(key,data[key]))
+    # data = ResumeParser(cv_path).get_extracted_data()
     
-    result=Tk()
-  #  result.geometry('700x550')
-    result.overrideredirect(False)
-    result.geometry("{0}x{1}+0+0".format(result.winfo_screenwidth(), result.winfo_screenheight()))
-    result.configure(background='White')
-    result.title("Predicted Personality")
+    # for key in data.keys():
+    #     if data[key] is not None:
+    #         print(str(key.title()))
+            # print(str('{} : {}'.format(check_type(key.title()),check_type(data[key]))))
+            # Label(result, text = str('{} : {}'.format(check_type(key.title()),check_type(data[key]))), foreground='black', bg='white', anchor='w', width=60).pack(fill=BOTH)
+    # print("perdicted personality: "+personality)
     
-    #Title
-    titleFont = font.Font(family='Arial', size=40, weight='bold')
-    Label(result, text="Result - Personality Prediction", foreground='green', bg='white', font=titleFont, pady=10, anchor=CENTER).pack(fill=BOTH)
+#     try:
+#         del data['name']
+#         if len(data['mobile_number'])<10:
+#             del data['mobile_number']
+#     except:
+#         pass
     
-    Label(result, text = str('{} : {}'.format("Name:", aplcnt_name.get())).title(), foreground='black', bg='white', anchor='w').pack(fill=BOTH)
-    Label(result, text = str('{} : {}'.format("Age:", age)), foreground='black', bg='white', anchor='w').pack(fill=BOTH)
-    for key in data.keys():
-        if data[key] is not None:
-            Label(result, text = str('{} : {}'.format(check_type(key.title()),check_type(data[key]))), foreground='black', bg='white', anchor='w', width=60).pack(fill=BOTH)
-    Label(result, text = str("perdicted personality: "+personality).title(), foreground='black', bg='white', anchor='w').pack(fill=BOTH)
-    
-    quitBtn = Button(result, text="Exit", command =lambda:  result.destroy()).pack()
-    
-    terms_mean = """
-# Openness:
-    People who like to learn new things and enjoy new experiences usually score high in openness. Openness includes traits like being insightful and imaginative and having a wide variety of interests.
+#     print("\n############# Resume Parsed Data #############\n")
 
-# Conscientiousness:
-    People that have a high degree of conscientiousness are reliable and prompt. Traits include being organised, methodic, and thorough.
-
-# Extraversion:
-    Extraversion traits include being; energetic, talkative, and assertive (sometime seen as outspoken by Introverts). Extraverts get their energy and drive from others, while introverts are self-driven get their drive from within themselves.
-
-# Agreeableness:
-    As it perhaps sounds, these individuals are warm, friendly, compassionate and cooperative and traits include being kind, affectionate, and sympathetic. In contrast, people with lower levels of agreeableness may be more distant.
-
-# Neuroticism:
-    Neuroticism or Emotional Stability relates to degree of negative emotions. People that score high on neuroticism often experience emotional instability and negative emotions. Characteristics typically include being moody and tense.    
-"""
+#     for key in data.keys():
+#         if data[key] is not None:
+#             print('{} : {}'.format(key,data[key]))
     
-    Label(result, text = terms_mean, foreground='green', bg='white', anchor='w', justify=LEFT).pack(fill=BOTH)
+#     result=Tk()
+#   #  result.geometry('700x550')
+#     result.overrideredirect(False)
+#     result.geometry("{0}x{1}+0+0".format(result.winfo_screenwidth(), result.winfo_screenheight()))
+#     result.configure(background='White')
+#     result.title("Predicted Personality")
+    
+#     #Title
+#     titleFont = font.Font(family='Arial', size=40, weight='bold')
+#     Label(result, text="Result - Personality Prediction", foreground='green', bg='white', font=titleFont, pady=10, anchor=CENTER).pack(fill=BOTH)
+    
+#     Label(result, text = str('{} : {}'.format("Name:", aplcnt_name.get())).title(), foreground='black', bg='white', anchor='w').pack(fill=BOTH)
+#     Label(result, text = str('{} : {}'.format("Age:", age)), foreground='black', bg='white', anchor='w').pack(fill=BOTH)
+#     for key in data.keys():
+#         if data[key] is not None:
+#             Label(result, text = str('{} : {}'.format(check_type(key.title()),check_type(data[key]))), foreground='black', bg='white', anchor='w', width=60).pack(fill=BOTH)
+#     Label(result, text = str("perdicted personality: "+personality).title(), foreground='black', bg='white', anchor='w').pack(fill=BOTH)
+    
+#     quitBtn = Button(result, text="Exit", command =lambda:  result.destroy()).pack()
+    
+#     terms_mean = """
+# # Openness:
+#     People who like to learn new things and enjoy new experiences usually score high in openness. Openness includes traits like being insightful and imaginative and having a wide variety of interests.
 
-    result.mainloop()
+# # Conscientiousness:
+#     People that have a high degree of conscientiousness are reliable and prompt. Traits include being organised, methodic, and thorough.
+
+# # Extraversion:
+#     Extraversion traits include being; energetic, talkative, and assertive (sometime seen as outspoken by Introverts). Extraverts get their energy and drive from others, while introverts are self-driven get their drive from within themselves.
+
+# # Agreeableness:
+#     As it perhaps sounds, these individuals are warm, friendly, compassionate and cooperative and traits include being kind, affectionate, and sympathetic. In contrast, people with lower levels of agreeableness may be more distant.
+
+# # Neuroticism:
+#     Neuroticism or Emotional Stability relates to degree of negative emotions. People that score high on neuroticism often experience emotional instability and negative emotions. Characteristics typically include being moody and tense.    
+# """
+    
+#     Label(result, text = terms_mean, foreground='green', bg='white', anchor='w', justify=LEFT).pack(fill=BOTH)
+
+#     result.mainloop()
     
 
 def perdict_person():
